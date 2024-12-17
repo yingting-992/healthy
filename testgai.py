f51fd4c31d9b9c0c7693b7f28b28f5a36995c4ec
shujuan1015 <C112156104@nkust.edu.tw>
2024-12-17 15:06:42 +0800

    a

--- testgai.py
+++ testgai.py
@@ -2,9 +2,8 @@ import os
 import google.generativeai as generativeai
 import dotenv
 
-dotenv.load_dotenv
+dotenv.load_dotenv()
 
-api_key = os.getenv("gemini_api_key")
-generativeai.configure(api_key=api_key)
+generativeai.configure(api_key=os.getenv("gemini_api_key"))
 response = generativeai.GenerativeModel("gemini-2.0-flash-exp").generate_content("你所得到的資訊最新的是什麼時候的?")
 print(response.text)
